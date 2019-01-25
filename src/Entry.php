267e6524e06f76a1bcef0d3da1266abb97f5d7e9
Dmytro S <freelancerua@protonmail.com>
2019-01-25 12:50:08 +0200

    [*] - Conver tab to spaces

--- src/Entry.php
+++ src/Entry.php
@@ -15,120 +15,120 @@ namespace TestProject;
  */
 class Entry
 {
-	/**
-	 * Path to file/catalog
-	 * @var string
-	 */
-	protected $path;
+    /**
+     * Path to file/catalog
+     * @var string
+     */
+    protected $path;
 
-	/**
-	 * Catelog's/file's name
-	 * @var string
-	 */
-	protected $name;
+    /**
+     * Catelog's/file's name
+     * @var string
+     */
+    protected $name;
 
-	/**
-	 * File created date 
-	 * @see time()
-	 * @var int
-	 */
-	protected $date;
+    /**
+     * File created date 
+     * @see time()
+     * @var int
+     */
+    protected $date;
 
-	/**
-	 * Is file or catalog entry
-	 * @var bool
-	 */
-	protected $is_file;
+    /**
+     * Is file or catalog entry
+     * @var bool
+     */
+    protected $is_file;
 
-	/**
-	 * Child entries storage
-	 * @var Entry[]
-	 */
-	protected $entries = [];
+    /**
+     * Child entries storage
+     * @var Entry[]
+     */
+    protected $entries = [];
 
-	/**
-	 * @param string $path Path to file/catalog entry
-	 * @param string $name Name of file/catalog entry
-	 * @param int $date Date of file/catalog has been created entry
-	 * @param bool $is_file Is file(true) or catelog(false) entry
-	 */
-	public function __construct(string $path, string $name, int $date, bool $is_file = true)
-	{
-		$this->path = $path;
-		$this->name = $name;
-		$this->date = $date;
-		$this->is_file = $is_file;
-	}
+    /**
+     * @param string $path Path to file/catalog entry
+     * @param string $name Name of file/catalog entry
+     * @param int $date Date of file/catalog has been created entry
+     * @param bool $is_file Is file(true) or catelog(false) entry
+     */
+    public function __construct(string $path, string $name, int $date, bool $is_file = true)
+    {
+        $this->path = $path;
+        $this->name = $name;
+        $this->date = $date;
+        $this->is_file = $is_file;
+    }
 
-	/**
-	 *	Build names array recursive
-	 */
-	protected function walkRecursive() : array
-	{
-		$names = [];
+    /**
+     *  Build names array recursive
+     */
+    protected function walkRecursive() : array
+    {
+        $names = [];
 
-		// Add self name to result calculation
-		$names[] = $this->name;
-		foreach ($this->entries as $entry)
-		{
-			if($entry->is_file) 
-			{
-				// Just add file name
-				$names[] = $entry->name;
-			} 
-			else 
-			{
-				// Walk through sub catalog recursively
-				$names = array_merge($names, $entry->walkRecursive());
-			}
-		}
+        // Add self name to result calculation
+        $names[] = $this->name;
+        foreach ($this->entries as $entry)
+        {
+            if($entry->is_file) 
+            {
+                // Just add file name
+                $names[] = $entry->name;
+            } 
+            else 
+            {
+                // Walk through sub catalog recursively
+                $names = array_merge($names, $entry->walkRecursive());
+            }
+        }
 
-		return $names;
-	} 
+        return $names;
+    } 
 
-	/**
-	 * Least one key will be in result
- 	 * And here we ensure that pointer will be on first element
- 	 * But if you need to rewind array use reset($names_counted)
- 	 * ---------------------------------------------------------
- 	 * In PHP 7.3 and higher:
- 	 * return array_key_first($names_counted);
- 	 * This function no matter of current array pointer
- 	 * 
-	 * @return string Most using name of files and catalgos in catalog and child catalogs
-	 */
-	public function getMostUsedName() : string
- 	{
- 		$names_counted = array_count_values($this->walkRecursive());
- 		arsort($names_counted);
- 		return key($names_counted); 	
-	}
+    /**
+     * Least one key will be in result
+     * And here we ensure that pointer will be on first element
+     * But if you need to rewind array use reset($names_counted)
+     * ---------------------------------------------------------
+     * In PHP 7.3 and higher:
+     * return array_key_first($names_counted);
+     * This function no matter of current array pointer
+     * 
+     * @return string Most using name of files and catalgos in catalog and child catalogs
+     */
+    public function getMostUsedName() : string
+    {
+        $names_counted = array_count_values($this->walkRecursive());
+        arsort($names_counted);
+        return key($names_counted);     
+    }
 
-	/**
-	 * Add new child entry
-	 * @param Entry $entry
-	 */
-	public function addChildEntry(Entry $entry) : void
-	{
-		// Probably here exception would better then silent skip
-		if(!$this->is_file) {
-			$this->entries[] = $entry;
-		}
-	}
+    /**
+     * Add new child entry
+     * @param Entry $entry
+     */
+    public function addChildEntry(Entry $entry) : void
+    {
+        // Probably here exception would better then silent skip
+        if(!$this->is_file) {
+            $this->entries[] = $entry;
+        }
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function __invoke() : string
-	{
-		return $this->getMostUsedName();
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke() : string
+    {
+        return $this->getMostUsedName();
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function __toString() : string
-	{
-		return $this->path . DIRECTORY_SEPARATOR . $this->name;
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString() : string
+    {
+        return $this->path . DIRECTORY_SEPARATOR . $this->name;
+    }
 }
--- test/EntryTest.php
+++ test/EntryTest.php
@@ -7,15 +7,15 @@ use TestProject\Entry;
 
 class EntryTest extends TestCase
 {
-	/**
-	 * Test __toString magic
-	 */
+    /**
+     * Test __toString magic
+     */
     public function testCanBeUsedAsString(): void
     {
-    	$path = 'path';
-    	$name = 'test_name'; 
+        $path = 'path';
+        $name = 'test_name'; 
 
-    	$this->assertEquals(
+        $this->assertEquals(
             $path . DIRECTORY_SEPARATOR . $name,
             new Entry('path', $name, time(), false)
         );
@@ -27,28 +27,28 @@ class EntryTest extends TestCase
      */
     public function testMostUsedName()
     {
-    	// Catalogs
-    	$catalog = new Entry('path', 'name', time(), false);
-		$catalog_level_1 = new Entry('path', 'name2', time(), false);
-		$catalog_level_1_1 = new Entry('path', 'name3', time(), false);
+        // Catalogs
+        $catalog = new Entry('path', 'name', time(), false);
+        $catalog_level_1 = new Entry('path', 'name2', time(), false);
+        $catalog_level_1_1 = new Entry('path', 'name3', time(), false);
 
-		// Files
-		$file_1_catalog_level_1 = new Entry('path', 'name2', time());
-		$file_2_catalog_level_1 = new Entry('path', 'name1', time());
-		$file_1_catalog_level_1_1 = new Entry('path', 'name1', time());
-		$file_1_catalog = new Entry('path', 'name1', time());
+        // Files
+        $file_1_catalog_level_1 = new Entry('path', 'name2', time());
+        $file_2_catalog_level_1 = new Entry('path', 'name1', time());
+        $file_1_catalog_level_1_1 = new Entry('path', 'name1', time());
+        $file_1_catalog = new Entry('path', 'name1', time());
 
-		// Add as child elements
-		$catalog_level_1_1->addChildEntry($file_1_catalog_level_1_1);
-		$catalog_level_1->addChildEntry($file_1_catalog_level_1);
-		$catalog_level_1->addChildEntry($file_2_catalog_level_1);
-		$catalog_level_1->addChildEntry($catalog_level_1_1);
-		$catalog->addChildEntry($catalog_level_1);
-		$catalog->addChildEntry($file_1_catalog);
+        // Add as child elements
+        $catalog_level_1_1->addChildEntry($file_1_catalog_level_1_1);
+        $catalog_level_1->addChildEntry($file_1_catalog_level_1);
+        $catalog_level_1->addChildEntry($file_2_catalog_level_1);
+        $catalog_level_1->addChildEntry($catalog_level_1_1);
+        $catalog->addChildEntry($catalog_level_1);
+        $catalog->addChildEntry($file_1_catalog);
 
-		// Make test
-		$this->assertEquals(
-			'name1', $catalog()
-		);
+        // Make test
+        $this->assertEquals(
+            'name1', $catalog()
+        );
     }
 }
